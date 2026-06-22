b8bbad3ee2ee0ecaf83be92c42d2c7768c241c4b
Roy Portas <royportas@gmail.com>
2026-06-23 09:05:46 +1000

    Setup storybook

---
+++ packages/design-system/.storybook/preview.tsx
@@ -0,0 +1,14 @@
+import { definePreview } from "@storybook/react-vite";
+
+export default definePreview({
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+
+  addons: []
+});
--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,12 +1,38 @@
 import react from "@vitejs/plugin-react";
+import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
+import packageJson from "./package.json";
+
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.tsx",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rolldownOptions: {
+      external: [
+        // Mark all peer dependencies as external
+        ...Object.keys(packageJson.peerDependencies),
+        // Also mark the JSX runtime as external too
+        "react/jsx-runtime",
+      ],
+    },
+  },
   server: {
     port: 3000,
   },
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      // Include just the source files for type generation
+      include: ["src"],
+      // Set the rootDir as "src", so "src/index.tsx" becomes "dist/index.d.ts"
+      compilerOptions: { rootDir: "src" },
+    }),
+  ],
 });

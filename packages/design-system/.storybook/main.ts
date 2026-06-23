293ecfd30c8b2706bac47dcdfe402525b5e6d1a1
Roy Portas <royportas@gmail.com>
2026-06-23 10:10:14 +1000

    Strip out the vite build options for storybook dev/build

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -7,4 +7,14 @@ export default defineMain({
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [],
+  // Strip out the build-only parts of the Vite config
+  viteFinal: (config) => {
+    // Don't include the dts plugin
+    config.plugins = config.plugins?.filter(
+      (plugin) => !(plugin && "name" in plugin && plugin.name === "unplugin-dts"),
+    );
+    // Delete the build configuration
+    delete config.build;
+    return config;
+  },
 });
